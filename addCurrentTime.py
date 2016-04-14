1d60917095821a5e2284f827b18d1571ed51698f
Yan Yan34 Li <liyan34@lenovo.com>
2016-04-14 19:58:33 +0800

    cmspageaction

---
+++ addCurrentTime.py
@@ -0,0 +1,11 @@
+import sublime
+import sublime_plugin
+import datetime
+
+class AddCurrentTimeCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        self.view.run_command("insert_snippet",
+            {
+                "contents": "%s" % datetime.datetime.now().strftime("%Y-%m-%d")
+            }
+        )
